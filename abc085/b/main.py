cbb2d078d514ea20de764d71cd083255820d2a36
tomoyamizukuro <salvatoleyohane@gmail.com>
2022-03-25 18:23:49 +0900

    UPDATE Atcoder Problems

--- abc085/b/main.py
+++ abc085/b/main.py
@@ -1,2 +1,17 @@
 import math
 from collections import defaultdict
+
+N = int(input())
+A = []
+for _ in range(N):
+    A.append(int(input()))
+A.sort()
+
+count = 0
+mochi = 0
+for i in range(N):
+    if A[i] > mochi:
+        count += 1
+    mochi = A[i]
+
+print(count)
--- abc085/c/main.py
+++ abc085/c/main.py
@@ -1,2 +1,21 @@
 import math
 from collections import defaultdict
+
+N, Y = map(int, input().split())
+x = -1
+y = -1
+z = -1
+for a in range(N+1):
+    for b in range(N+1):
+        c = N-(a+b)
+        total = 10000*a + 5000*b + 1000*c
+        if 0 <= c <= 2000 and total == Y:
+            x = a
+            y = b
+            z = c
+            print(x, y, z)
+            exit()
+        else:
+            continue
+
+print(x, y, z)
