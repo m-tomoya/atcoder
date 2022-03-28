1ecf1e8680b836c1a24b83b046a716b043f74c78
tomoyamizukuro <salvatoleyohane@gmail.com>
2022-03-28 23:11:38 +0900

    UPDATE Atcoder Problems

--- aojTutorial/8C_countingCharacter.py
+++ aojTutorial/8C_countingCharacter.py
@@ -1,12 +1,13 @@
-words = input()
-words = words.replace(" ", "").lower()
+import sys
+texts = sys.stdin.read()
+texts = texts.replace(" ", "").lower()
 list = "abcdefghijklmnopqrstuvwxyz"
 dict = dict()
 for i in range(len(list)):
     dict[list[i]] = 0
 
-for j in range(len(words)):
-    dict[words[j]] += 1
+for j in range(len(texts)):
+    dict[texts[j]] += 1
 
 for k in dict.keys():
     print(k + " : " + str(dict[k]))
